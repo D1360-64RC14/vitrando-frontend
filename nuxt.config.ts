5f619a422c3573dce932aef934d8d39a8adaddf0
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-11-16 22:26:12 -0300

    feat: populate nuxt config with route rules and app metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: {
-    enabled: true
+    enabled: true,
   },
-  css: ['~/assets/css/main.css'],
+  routeRules: {
+    "/": {
+      ssr: true,
+    },
+  },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Vitrando: A Vitrine Virtual para sua Loja",
+      meta: [
+        {
+          property: "og:title",
+          content: "Vitrando: A Vitrine Virtual para sua Loja",
+        },
+        {
+          name: "description",
+          property: "og:description",
+          content:
+            "Está procurando um lugar para anunciar facilmente seus produtos sem o trabalho de gerenciar todo um e-commerce? Então vamos juntos criar uma vitrine virtual para sua loja!",
+        },
+        {
+          property: "og:type",
+          content: "ecommerce",
+        },
+        {
+          property: "og:locale",
+          content: "pt_BR",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+});
