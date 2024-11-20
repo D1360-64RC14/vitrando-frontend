659108a6889cfde71535e940da9ac06e2e9796a8
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-11-20 19:26:02 -0300

    feat: centered layout

---
+++ layouts/Centered.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { Card } from "primevue";
+import HeaderLogo from "~/components/header/HeaderLogo.vue";
+import StoreHeader from "~/components/StoreHeader.vue";
+</script>
+
+<template>
+  <NuxtLayout name="base">
+    <template #header>
+      <StoreHeader>
+        <HeaderLogo />
+      </StoreHeader>
+    </template>
+
+    <div class="h-full flex flex-col justify-center items-center">
+      <Card class="max-w-md w-full">
+        <template #content>
+          <slot></slot>
+        </template>
+      </Card>
+    </div>
+  </NuxtLayout>
+</template>
