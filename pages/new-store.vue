8b6abd866b271f607f2d14e3c42113ea488733d2
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-12-19 20:23:57 -0300

    feat: new-store base page

---
+++ pages/new-store.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const profileStore = useMyProfileStore();
+</script>
+
+<template>
+  <NuxtLayout name="base">
+    <template #header>
+      <StoreHeader>
+        <HeaderLogo />
+        <HeaderAccount v-if="profileStore.isLoggedIn" />
+        <HeaderSignIn v-else />
+      </StoreHeader>
+    </template>
+
+    <div class="flex justify-center">
+      <Card>
+        <div></div>
+      </Card>
+    </div>
+  </NuxtLayout>
+</template>
